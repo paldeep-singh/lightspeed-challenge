970134987b522375186f227b67b6122aa0858922
Paldeep Singh <paldeepsingh21@gmail.com>
2024-09-29 12:35:24 +1000

    Improve menu styling

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -6,6 +6,7 @@ import { Button } from "./Button";
 import { theme } from "../utils/theme";
 
 const ITEM_VERTICAL_PADDING = 12;
+const ITEM_HORIZONTAL_PADDING = 32;
 
 const styles = StyleSheet.create({
   listContainer: {
@@ -13,20 +14,26 @@ const styles = StyleSheet.create({
     alignContent: "center",
     width: "100%",
     flexGrow: 1,
-    padding: 32,
   },
   buttonContainer: {
+    flex: 1,
     paddingVertical: ITEM_VERTICAL_PADDING,
     backgroundColor: theme.background,
+    paddingHorizontal: ITEM_HORIZONTAL_PADDING,
   },
   priceTotal: {
     justifyContent: "space-between",
     flexDirection: "row",
     borderTopColor: theme.border,
     borderTopWidth: 1,
-    padding: 16,
+    paddingHorizontal: ITEM_HORIZONTAL_PADDING,
+    alignItems: "center",
+    minHeight: "15%",
   },
-  itemContainer: { paddingVertical: ITEM_VERTICAL_PADDING },
+  itemContainer: {
+    paddingVertical: ITEM_VERTICAL_PADDING,
+    paddingHorizontal: ITEM_HORIZONTAL_PADDING,
+  },
 });
 
 interface IMenuProps {
